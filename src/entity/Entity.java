5d143f7012d311f9b71fca363bc9bb0c640049fd
George Cocks <georgecocks01@gmail.com>
2023-10-27 13:25:53 +0100

    Added in player sprites and updated code to load them

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,8 +1,15 @@
 package entity;
+
+import java.awt.image.BufferedImage;
+
 /* Parent/Super class for all players, monsters and NPCs */
 public class Entity {
 
   public int x, y;
   public int speed;
 
+  public BufferedImage forward, forward1, forward2, backward, backward1,
+    backward2, left, left1, right, right1;
+
+  public String direction;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import main.GamePanel;
 import main.KeyHandler;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Player extends Entity {
 
@@ -14,6 +17,7 @@ public class Player extends Entity {
     this.gp = gp;
     this.keyH = keyH;
     setDefaultValues();
+    getPlayerImage();
   }
 
   // Constructor
@@ -22,25 +26,70 @@ public class Player extends Entity {
     x = 100;
     y = 100;
     speed = 2; // could use gp.playerSpeed instead
+    direction = "forward";
+  }
+
+  public void getPlayerImage() {
+    System.out.println("Loading player images");
+    try {
+      forward = ImageIO.read(getClass().getResourceAsStream("/player/player_standing.png"));
+      forward1 = ImageIO.read(getClass().getResourceAsStream("/player/player_walking_forward_1.png"));
+      forward2 = ImageIO.read(getClass().getResourceAsStream("/player/player_walking_forward_2.png"));
+      backward = ImageIO.read(getClass().getResourceAsStream("/player/player_standing_back.png"));
+      backward1 = ImageIO.read(getClass().getResourceAsStream("/player/player_walking_away_1.png"));
+      backward2 = ImageIO.read(getClass().getResourceAsStream("/player/player_walking_away_2.png"));
+      left = ImageIO.read(getClass().getResourceAsStream("/player/player_standing_left.png"));
+      left1 = ImageIO.read(getClass().getResourceAsStream("/player/player_walking_left.png"));
+      right = ImageIO.read(getClass().getResourceAsStream("/player/player_standing_right.png"));
+      right1 = ImageIO.read(getClass().getResourceAsStream("/player/player_walking_right.png"));
+      System.out.println("Player images loaded");
+    } catch(IOException e) {
+      System.out.println("Player images not loaded");
+      e.printStackTrace();
+    }
   }
 
   public void update() {
     if(keyH.upPressed == true) {
+      direction = "backward";
       y -= speed;
     }
     if (keyH.downPressed == true) {
+      direction = "forward";
       y += speed;
     }
     if (keyH.leftPressed == true) {
+      direction = "left";
       x -= speed;
     }
     if (keyH.rightPressed == true) {
+      direction = "right";
       x += speed;
     }
   }
 
   public void draw(Graphics2D g2) {
-    g2.setColor(Color.white); // makes rectangle white
-    g2.fillRect(x, y, gp.tileSize, gp.tileSize); // sets posn and size of rectangle
+    // g2.setColor(Color.white); // makes rectangle white
+    // g2.fillRect(x, y, gp.tileSize, gp.tileSize); // sets posn and size of rectangle
+
+    BufferedImage image = null;
+
+    switch(direction) {
+      case "forward":
+        image = forward;
+        break;
+      case "backward":
+        image = backward;
+        break;
+      case "left":
+        image = left;
+        break;
+      case "right":
+        image = right;
+        break;
+    }
+
+    g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null); // image observer = null
+
   }
 }
